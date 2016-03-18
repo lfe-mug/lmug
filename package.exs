1d210e31633a6f1cb1e7f9b06bb784becef4d97a
Duncan McGreggor <oubiwann@gmail.com>
2016-03-17 23:02:46 -0500

    Removed EXPM.

--- package.exs
+++
@@ -1,7 +0,0 @@
-Expm.Package.new(
-  name: "lmug",
-  description: "lmug DESCRIPTION",
-  version: "0.0.1",
-  keywords: ["LFE", "Lisp", "Library", "API"],
-  maintainers: [[name: "YOUR NAME", email: "YOUR@EMAIL.com"]],
-  repositories: [[github: "YOUR_GITHUB_NAME/lmug"]])
